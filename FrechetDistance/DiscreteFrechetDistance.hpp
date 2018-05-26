13719abd420901d059ccc4904c516b45a6297aac
Yaghyavardhan singh khangarot <yaghya.jaipur@gmail.com>
2018-05-27 00:03:38 +0530

    Update Discret Frechet Distance

--- FrechetDistance/DiscreteFrechetDistance.hpp
+++ FrechetDistance/DiscreteFrechetDistance.hpp
@@ -16,12 +16,7 @@
 #include <boost/geometry/geometries/linestring.hpp>
 #include <boost/geometry/geometries/point_xy.hpp>
 #include <boost/geometry/geometries/polygon.hpp>
-#include <boost/array.hpp>
-#include <boost/numeric/ublas/matrix.hpp>
-#include <boost/numeric/ublas/io.hpp>
-
-#include <boost/geometry/geometries/register/linestring.hpp>
-#include <boost/geometry/geometries/adapted/c_array.hpp>
+#include <boost/range.hpp>
 
 
 namespace bnu = boost::numeric::ublas;
@@ -64,26 +59,26 @@ static inline double FrechetDistance(LineString ls1,LineString ls2)
  	std::cout << "size of linestring2 ="<< b << std::endl;
 
  	//bnu::matrix<double>  CoupMat(a+1,b+1,-1);
-  double CoupMat[a+1][b+1];
-  for(unsigned int i = 0; i <=a; i++)
-    for(unsigned int j = 0; j <=b; j++)
+  double CoupMat[a][b];
+  for(unsigned int i = 0; i <a; i++)
+    for(unsigned int j = 0; j <b; j++)
       CoupMat[i][j] = -1;
     
  	//findin the Coupling
  	//calling Recursion to get the coupling distance
  	double NonFeasible=-100;
- 	for(unsigned int i=1;i<=a;i++)
+ 	for(unsigned int i=0;i<a;i++)
  	{
- 		for(unsigned  int j=1;j<=b;j++)
+ 		for(unsigned  int j=0;j<b;j++)
  		{
- 			if(i==1 && j==1)
- 				CoupMat[i][j]= Distance(ls1[i],ls2[j]);
- 			else if(i==1 && j>1)
- 				CoupMat[i][j]=max(CoupMat[i][j-1],Distance(ls1[i],ls2[j]));
- 			else if(i>1 && j==1)
- 				CoupMat[i][j]=max(CoupMat[i-1][j],Distance(ls1[i],ls2[j]));
- 			else if(i>1 && j>1)
- 				CoupMat[i][j]=max(min(min(CoupMat[i][j-1],CoupMat[i-1][j]),CoupMat[i-1][j-1]),Distance(ls1[i],ls2[j]));
+ 			if(i==0 && j==0)
+ 				CoupMat[i][j]= Distance(bg::range::at(ls1,i),bg::range::at(ls2,j));
+ 			else if(i==0 && j>0)
+ 				CoupMat[i][j]=max(CoupMat[i][j-1],Distance(bg::range::at(ls1,i),bg::range::at(ls2,j)));
+ 			else if(i>0 && j==0)
+ 				CoupMat[i][j]=max(CoupMat[i-1][j],Distance(bg::range::at(ls1,i),bg::range::at(ls2,j)));
+ 			else if(i>0 && j>0)
+ 				CoupMat[i][j]=max(min(min(CoupMat[i][j-1],CoupMat[i-1][j]),CoupMat[i-1][j-1]),Distance(bg::range::at(ls1,i),bg::range::at(ls2,j)));
  			else
  				CoupMat[i][j]=NonFeasible;
  		}
@@ -91,17 +86,16 @@ static inline double FrechetDistance(LineString ls1,LineString ls2)
 
  	//Print CoupLing Matrix
 
- 	for(unsigned int i = 1; i <=a; i++)
+ 	for(unsigned int i = 0; i <a; i++)
   {
-    for(unsigned int j = 1; j <=b; j++)
+    for(unsigned int j = 0; j <b; j++)
       std::cout << CoupMat[i][j] << " ";
     std::cout << std::endl;
   }
 
- 	//std::cout << CoupMat << std::endl;
 	
 	//Final Coupling Distance
-	Dis=CoupMat[a][b];
+	Dis=CoupMat[a-1][b-1];
 	
 	return Dis;
 }
