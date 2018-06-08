477ec6c2fd293cc0bc467a832f8fb81beddf255e
Yaghyavardhan singh khangarot <yaghya.jaipur@gmail.com>
2018-06-09 03:26:12 +0530

    Update Discrete Hausdorff Distance for LineStrings

--- HausdorffDistance/DiscreteHausdorffDistance.hpp
+++ HausdorffDistance/DiscreteHausdorffDistance.hpp
@@ -23,44 +23,46 @@
 #include <boost/mpl/assert.hpp>
 
 
-namespace bg = boost::geometry;
-
 
 //Calculating distance as per the Strategy Type
 template <typename Point>
 static inline double Distance(Point const& p1, Point const& p2)
 {
-    typedef typename bg::strategy::distance::services::default_strategy
+    typedef typename boost::geometry::strategy::distance::services::default_strategy
               <
-                  bg::point_tag, bg::point_tag,
+                  boost::geometry::point_tag, boost::geometry::point_tag,
                   Point, Point
               >::type strategy_type;
 
-    return bg::distance(p1, p2, strategy_type());
+    return boost::geometry::distance(p1, p2, strategy_type());
 }
 
 
 namespace boost { namespace geometry {
-namespace HausdorffDistance
+namespace algorithms
 {
 
-template<typename LineString>
-static inline double HausDist(LineString ls1,LineString ls2)
+template<typename Geometry1 ,typename Geometry2>
+static inline double hausdorff_distance(Geometry1 ls1,Geometry2 ls2)
 {
-	double DisMax=0,DisMin;
-  double infinite = std::numeric_limits<double>::max();
-	unsigned int  a = boost::size(ls1);
+  typedef typename distance_result<Geometry1, Geometry2>::type result_type;
+  typedef typename boost::range_size<Geometry1>::type size_type;
+
+	result_type DisMax=0,DisMin;
+  result_type infinite = std::numeric_limits<double>::max();
+	
+  size_type  a = boost::size(ls1);
  	std::cout <<"size of linestring1 ="<< a << std::endl;
- 	unsigned int  b = boost::size(ls2);
+ 	size_type  b = boost::size(ls2);
  	std::cout << "size of linestring2 ="<< b << std::endl;
 
  	//findin the Coupling
- 	for(unsigned int i=0;i<a;i++)
+ 	for(size_type i=0;i<a;i++)
  	{
-    DisMin= std::numeric_limits<double>::max() ;
- 		for(unsigned  int j=0;j<b;j++)
+    DisMin= std::numeric_limits<result_type>::max() ;
+ 		for(size_type j=0;j<b;j++)
  		{
- 			double DisTemp = Distance(bg::range::at(ls1,i),bg::range::at(ls2,j));
+ 			result_type DisTemp = Distance(boost::geometry::range::at(ls1,i),boost::geometry::range::at(ls2,j));
       if(DisTemp < DisMax)
         break; //Early Break
       if(DisTemp < DisMin)
@@ -76,7 +78,7 @@ static inline double HausDist(LineString ls1,LineString ls2)
 	return DisMax;
 };
 
-} // Hausdorff Distance
+} // algorithms
 
 }} // namespace boost::geometry
 
