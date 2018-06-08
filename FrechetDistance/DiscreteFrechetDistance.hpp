cffa9d9ababb840df5147fef62adb90ea240e5fe
Yaghyavardhan singh khangarot <yaghya.jaipur@gmail.com>
2018-06-09 03:19:32 +0530

    Update Discrete Frechet Distance for Linestrings

--- FrechetDistance/DiscreteFrechetDistance.hpp
+++ FrechetDistance/DiscreteFrechetDistance.hpp
@@ -24,91 +24,75 @@
 
 
 
-namespace bg = boost::geometry;
-
-
-template <typename DataType>
-static inline DataType max(DataType a,DataType b)
-{
-	return (a>b)?a:b;
-}
-
-template <typename ElementType>
-static inline ElementType min(ElementType a,ElementType b)
-{	
-	return (a<b)?a:b;
-}
-
 
 //Calculating distance as per the Strategy Type
 template <typename Point>
 static inline double Distance(Point const& p1, Point const& p2)
 {
-    typedef typename bg::strategy::distance::services::default_strategy
+    typedef typename boost::geometry::strategy::distance::services::default_strategy
               <
-                  bg::point_tag, bg::point_tag,
+                  boost::geometry::point_tag, boost::geometry::point_tag,
                   Point, Point
               >::type strategy_type;
 
-    return bg::distance(p1, p2, strategy_type());
+    return boost::geometry::distance(p1, p2, strategy_type());
 }
 
 
 namespace boost { namespace geometry {
 
-namespace FrechetDistance{
+namespace algorithms{
 
-template<typename LineString>
-static inline double FrechDist(LineString ls1,LineString ls2)
+template<typename Geometry1,typename Geometry2>
+static inline double frechet_distance(Geometry1 const& ls1,Geometry2 const& ls2)
 {
-	double Dis=0;
-	unsigned int  a = boost::size(ls1);
- 	std::cout <<"size of linestring1 ="<< a << std::endl;
- 	unsigned int  b = boost::size(ls2);
- 	std::cout << "size of linestring2 ="<< b << std::endl;
+  typedef typename distance_result<Geometry1, Geometry2>::type result_type;
+  typedef typename boost::range_size<Geometry1>::type size_type;
+	
+  size_type  a = boost::size(ls1);
+  size_type  b = boost::size(ls2);
 
- 	//bnu::matrix<double>  CoupMat(a+1,b+1,-1);
-  double CoupMat[a][b];
-  for(unsigned int i = 0; i <a; i++)
-    for(unsigned int j = 0; j <b; j++)
-      CoupMat[i][j] = -1;
+  result_type const uninitialized = -1;
+  result_type const not_feasible = -100;
+
+ 	//Coupling Matrix CoupMat(a+1,b+1,-1);
+  result_type CoupMat[a][b];
+  for(size_type i = 0; i <a; i++)
+    for(size_type j = 0; j <b; j++)
+      CoupMat[i][j] = uninitialized;
     
  	//findin the Coupling
- 	double NonFeasible=-100;
- 	for(unsigned int i=0;i<a;i++)
+ 	for(size_type i=0;i<a;i++)
  	{
- 		for(unsigned  int j=0;j<b;j++)
+ 		for(size_type j=0;j<b;j++)
  		{
  			if(i==0 && j==0)
- 				CoupMat[i][j]= Distance(bg::range::at(ls1,i),bg::range::at(ls2,j));
+ 				CoupMat[i][j]= Distance(boost::geometry::range::at(ls1,i),boost::geometry::range::at(ls2,j));
  			else if(i==0 && j>0)
- 				CoupMat[i][j]=max(CoupMat[i][j-1],Distance(bg::range::at(ls1,i),bg::range::at(ls2,j)));
+ 				CoupMat[i][j]=(std::max)(CoupMat[i][j-1],Distance(boost::geometry::range::at(ls1,i),boost::geometry::range::at(ls2,j)));
  			else if(i>0 && j==0)
- 				CoupMat[i][j]=max(CoupMat[i-1][j],Distance(bg::range::at(ls1,i),bg::range::at(ls2,j)));
+ 				CoupMat[i][j]=(std::max)(CoupMat[i-1][j],Distance(boost::geometry::range::at(ls1,i),boost::geometry::range::at(ls2,j)));
  			else if(i>0 && j>0)
- 				CoupMat[i][j]=max(min(min(CoupMat[i][j-1],CoupMat[i-1][j]),CoupMat[i-1][j-1]),Distance(bg::range::at(ls1,i),bg::range::at(ls2,j)));
+ 				CoupMat[i][j]=(std::max)((std::min)((std::min)(CoupMat[i][j-1],CoupMat[i-1][j]),CoupMat[i-1][j-1]),Distance(boost::geometry::range::at(ls1,i),boost::geometry::range::at(ls2,j)));
  			else
- 				CoupMat[i][j]=NonFeasible;
+ 				CoupMat[i][j]=not_feasible;
  		}
  	}
 
  	//Print CoupLing Matrix
 
- 	for(unsigned int i = 0; i <a; i++)
+ 	for(size_type i = 0; i <a; i++)
   {
-    for(unsigned int j = 0; j <b; j++)
+    for(size_type j = 0; j <b; j++)
       std::cout << CoupMat[i][j] << " ";
     std::cout << std::endl;
   }
-
 	
 	//Final Coupling Distance
-	Dis=CoupMat[a-1][b-1];
-	
-	return Dis;
+	return CoupMat[a-1][b-1];
 };
 
-} // namespace Frechet Distance
+} // namespace algorithms
 
 }} // namespace boost::geometry
 
